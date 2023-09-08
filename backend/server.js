e38cf7d2308757caf7b1c2bb7f766876815fd789
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-09-09 02:09:17 +0300

    added .env file with PORT

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 
-
+// express app
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT
 
+// routes
 app.get('/', (req, res) => {
     res.json({message: 'Welcome to the app'})
 })
 
+
+// listen for requests
 app.listen(PORT, () => {
     console.log('Server is running on PORT: ', PORT)
 });
