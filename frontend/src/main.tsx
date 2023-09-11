9c9b941932f39de218b56dffe454cb483118be1d
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-09-11 23:53:36 +0300

    setting up routers with react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,7 @@
 import './index.css'
-
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './pages/Home.tsx';
-import About from './pages/About.tsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/about",
-    element: <About />
-  },
-]);
+import App from './App.tsx';
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <App />
 );
