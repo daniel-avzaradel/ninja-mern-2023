41a76dd780a72a23872c235a01f98b69ebc36efe
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-09-13 00:07:42 +0300

    text

--- frontend/src/components/WorkoutDetails.tsx
+++ frontend/src/components/WorkoutDetails.tsx
@@ -15,7 +15,7 @@ const WorkoutDetails = ({workout}: WorkoutProps) => {
       <h4>{workout.title}</h4>
       <p><strong>Load (Kg): </strong>{workout.load}</p>
       <p><strong>Reps: </strong>{workout.reps}</p>
-      <p>Created At: {workout.createdAt}</p>
+      <p>C{workout.createdAt}</p>
       </div>
     )
 }
