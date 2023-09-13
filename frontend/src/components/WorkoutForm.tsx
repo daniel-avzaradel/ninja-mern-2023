4077ef34ca9b2f1dd55ed6f9132a7c070175d74e
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-09-13 22:03:18 +0300

    removed unused imports

--- frontend/src/components/WorkoutForm.tsx
+++ frontend/src/components/WorkoutForm.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 const WorkoutForm = () => {
 
     const [title, setTitle] = useState('');
-    const [load, setLoad] = useState(0);
-    const [reps, setReps] = useState(0);
+    const [load, setLoad] = useState('');
+    const [reps, setReps] = useState('');
 
   return (
       <form className="create">
           <h3>Add a new Workout</h3>
+
           <label>Exercise Title:</label>
           <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
+
+          <label>Load (in Kgs):</label>
+          <input type="text" onChange={(e) => setLoad(e.target.value)} value={load} />
+
+          <label>Reps:</label>
+          <input type="text" onChange={(e) => setReps(e.target.value)} value={reps} />
+
+          <button>Add Workout</button>
     </form>
   )
 }
