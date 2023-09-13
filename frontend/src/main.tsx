4ac2a0bb776afcc3ffca3536e7f3a75851ded441
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2023-09-14 02:41:28 +0300

    creating context api to update global state

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,11 @@
 import './index.css'
 
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
+import {WorkoutContextProvider} from './context/WorkoutContext.jsx';
 import App from './App.tsx';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render (
+    <WorkoutContextProvider>
+        <App/>
+    </WorkoutContextProvider>
+);
